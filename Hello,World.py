d58bb97844e4b3e2c8c810ead30f62d7dadd08e6
RuLauf <dolgov.pavel007@gmail.com>
2025-03-19 18:55:10 +0000

    комит 4

--- Hello,World.py
+++ Hello,World.py
@@ -1,10 +1,10 @@
 print("Hello, World!")
 
 def fib(n):
-    a, b = 0, 1
-    for i in range(n):
-        if i > 0:
-            a, b = b, a + b
-    return a
+    fib_list = [0, 1]
+    for i in range(2, n):
+        fib_list.append(fib_list[i-1] + fib_list[i-2])
+    for num in fib_list:
+        print(num)
 
-print(fib(10))
+fib(10)
